436731c5bb5f61f808fa83f8d879ec9c9f3d5d51
Jan Klapetek <jan.klapetek@gmail.com>
2025-01-02 13:19:05 +0100

    new html added and delete Django

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuItems = document.querySelectorAll("#menu li a");
+    // Zvýraznění položek menu při najetí myší
+    const menuItems = document.querySelectorAll("nav ul li a");
 
     menuItems.forEach(item => {
         item.addEventListener("mouseenter", function() {
@@ -12,48 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
             item.style.backgroundColor = "";
         });
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+    // Nastavení aktuálního roku v patičce
     const currentYearElement = document.getElementById("current-year");
     const currentYear = new Date().getFullYear();
     currentYearElement.textContent = currentYear;
-});
 
-// funkce pro přechod na další nebo předchozí stránku Hearthstone
-document.addEventListener("DOMContentLoaded", () => {
-    const prevPageButton = document.getElementById("prevPage");
-    const nextPageButton = document.getElementById("nextPage");
-    let currentPage = new URLSearchParams(window.location.search).get("page") || 1;
-
-    prevPageButton.addEventListener("click", () => {
-        if (currentPage > 1) {
-            window.location.href = `/hearthstone/?page=${parseInt(currentPage) - 1}`;
-        }
-    });
-
-    nextPageButton.addEventListener("click", () => {
-        window.location.href = `/hearthstone/?page=${parseInt(currentPage) + 1}`;
-    });
-});
-
-// Přidání funkce potvrzení odeslání formuláře
-const contactForm = document.querySelector("#contact-form");
-if (contactForm) {
-    contactForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Zabráníme defaultnímu odeslání formuláře
-        alert("Váš e-mail byl úspěšně odeslán! Děkujeme za zprávu.");
-        contactForm.reset(); // Vyčistí formulář
-    });
-};
-
-document.addEventListener("DOMContentLoaded", function() {
+    // Přepínání tmavého režimu
     const darkModeToggle = document.getElementById("darkModeToggle");
     const body = document.body;
 
     darkModeToggle.addEventListener("click", function() {
         body.classList.toggle("dark-mode");
-        // Uložíme stav režimu do localStorage, aby se režim uchoval i po obnovení stránky
+
+        // Uložení režimu do localStorage
         if (body.classList.contains("dark-mode")) {
             localStorage.setItem("darkMode", "enabled");
         } else {
@@ -61,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Zkontrolujeme, jestli je režim již aktivní v localStorage při načítání stránky
+    // Načtení režimu při startu
     if (localStorage.getItem("darkMode") === "enabled") {
         body.classList.add("dark-mode");
     }
